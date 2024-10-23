32cce4cc9002eaa61915ae906944888ab68fe3c1
kwsmyy <kawashimayuya8080@gmail.com>
2024-10-23 14:51:45 +0900

    view port

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,8 @@ const notojp = Noto_Sans_JP({
 export const metadata: Metadata = {
   title: "memo gAmI",
   description: "memo gAmI",
+  viewport:
+    "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
 };
 
 export default function RootLayout({
